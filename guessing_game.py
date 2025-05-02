33243c82164cc79cd5f8bea30b26a7f731dcdb1e
nishaka36 <41-bis-0036@kdu.ac.lk>
2025-05-03 04:09:35 +0530

    Added hints after guess

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,19 @@
-import random 
-number = random.randint(1, 100) 
-print("Guess a number between 1 and 10") 
-guess = int(input()) 
-if guess == number: 
-print("You win!") 
-else: 
-print(f"Wrong! The number was {number}") 
+import random
+
+while True:
+    number = random.randint(1, 100)
+    print("Guess a number between 1 and 100")
+    guess = int(input())
+
+    if guess == number:
+        print("You win!")
+    else:
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        print(f"Wrong! The number was {number}")
+
+    print("Play again? (y/n)")
+    if input().lower() != 'y':
+        break
